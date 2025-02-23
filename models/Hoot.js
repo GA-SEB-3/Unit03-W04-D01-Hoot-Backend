36713e56fa4d550378166c8aefcee3399458a268
omarakamal <omaraakamal@gmail.com>
2025-02-23 18:49:38 +0300

    updated code

---
+++ models/Hoot.js
@@ -0,0 +1,36 @@
+const {Schema, model} = require("mongoose")
+
+const commentSchema = new Schema({
+    text:{
+        type:String,
+        required:true
+    },
+    author:{
+        type: Schema.Types.ObjectId,
+        ref:"User"
+       }
+},{timestamps:true})
+
+const hootSchema = new Schema({
+   title:{
+    type:String,
+    required:true
+   },
+   text:{
+    type:String,
+    required:true,
+    maxLength:240
+   },
+   category:{
+    type:String,
+    requied:true,
+    enum:['News', 'Sports', 'Games', 'Movies', 'Music', 'Television']
+   },
+   author:{
+    type: Schema.Types.ObjectId,
+    ref:"User"
+
+   },
+   comments:[commentSchema]
+},{timestamps:true}
+)
