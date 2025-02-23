68a05f404d3fb1fece0317de2ba26bf939fa0096
omarakamal <omaraakamal@gmail.com>
2025-02-23 19:08:17 +0300

    updated code

--- controllers/hoot.routes.js
+++ controllers/hoot.routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const Hoot = require("../models/Hoot")
+const verifyToken = require("../middleware/verify-token")
 
 router.get("/",(req,res)=>{
     res.json({message:"All Hoots!"})
